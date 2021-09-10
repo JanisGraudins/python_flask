c702b9189651aadfd78ebf37cf257f0bf2af3953
Jānis Graudiņš Adm <AdmBambuks@rd.rdad.riga.lv>
2021-09-10 23:13:46 +0300

    Izveidoti kontakti

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Sveika, Gauja!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Sveiks, jaukais pitons!"
+
+
+@app.route('/kontakti')
+def kontakti():
+    return "<html><h1>Kontakti</h1><p>Jānis  Graudiņš</p></html>"
+
+if __name__ == '__main__':
+    app.run(port=80, debug=True)
+
