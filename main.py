f16087191fb47d11c12ae4ff2feebad717f9525f
Jānis Graudiņš Adm <AdmBambuks@rd.rdad.riga.lv>
2021-09-11 00:19:47 +0300

    pievienota izvele

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ def home():
 def kontakti():
     return render_template('kontakti.html')
 
+@app.route('/par_mani')
+def par_mani():
+    return render_template('par_mani.html')
+
 if __name__ == '__main__':
     app.run(port=80, debug=True)
 
