72d0c337491fe8519272b1f9028c057340269473
Jānis Graudiņš Adm <AdmBambuks@rd.rdad.riga.lv>
2021-09-21 22:49:52 +0300

    pievienoju 404 lapu ar atsevisku css un layout

--- main.py
+++ main.py
@@ -126,6 +126,10 @@ def result():
     
 #         return render_template('pievienot.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+       return render_template('404.html'), 404
+
 if __name__ == '__main__':
     app.run(port=80, debug=True)
 
